814004824a55f120fad4095b56730136dd06d5eb
Prajakt <khawse.prajakt@gmail.com>
2020-07-04 09:44:05 -0500

    Initial project add

---
+++ src/main/java/com/cavirin/sfgpetclinic/SfgpetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.cavirin.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgpetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgpetclinicApplication.class, args);
+    }
+
+}
